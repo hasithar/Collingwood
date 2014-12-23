a02a3fd9d79a6be840e2ae0ada80ff747f6c11fb
hasithar <rukmal7@yahoo.com>
2014-12-23 17:50:38 +0530

    Added recovered Stylesheet
    Added the old Stylesheet. But no changes have made. Only the styles for
    homepage

---
+++ js/script.main.js
@@ -0,0 +1,44 @@
+// JavaScript Document
+$(document).ready(function(){
+    
+	//Homepage header slider
+	$("#home-header").bgswitcher({
+  		images: ["img/headers/home/img1.jpg",
+  		"img/headers/home/img2.jpg",
+  		"img/headers/home/img3.jpg", 
+  		"img/headers/home/img4.jpg"],
+  		duration: 1500
+	});
+	//end bgswitcher
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); //end document.ready()
+
+
+
+
+
+
+
+
+
+
+
+
